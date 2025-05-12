91ed7a375deac143f32abe9f6d71307284dfdd5f
noahzhy <noahzhy@gmail.com>
2025-05-13 01:19:32 +0800

    add: mbv4

--- main.py
+++ main.py
@@ -7,16 +7,29 @@ import torch.nn.functional as F
 
 from utils.tools import *
 from modules.xfeat import XFeat
+from modules.mbv4 import mobilenetv4_conv_small
+
+# disable cuda
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# cpu
+device = torch.device("cpu")
 
 
 if __name__ == "__main__":
-    model = XFeat().eval()
+    model = XFeat().eval().to(device)
     # input_shape = (1, 3, 1280, 720) # 7.78 GFLOPs
     input_shape = (1, 3, 640, 480)  # 2.65 GFLOPs
-    dummy_input = torch.randn(input_shape).cpu()
+    dummy_input = torch.randn(input_shape).to(device)
     out = model(dummy_input)
 
     for k, v in out[0].items():
         print(k, v.shape)
 
     count_parameters(model, input_shape)
+
+    model = mobilenetv4_conv_small(num_classes=10).eval().to(device)
+    count_parameters(model, input_shape)
+    
